0ea1a763de8275d23a9a6b75cddc23847a4bb7ae
Maxwell Anderson <maxwell.anderson.public@protonmail.com>
2022-03-18 16:33:30 -0600

    fix message length; trim stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use ferris_says::say;
-use std::{env, io::{stdout, BufWriter}};
+use std::{
+    env,
+    io::{stdout, BufWriter},
+};
 
 fn main() {
     let stdout = stdout();
+
     let message: String = env::args().skip(1).collect::<Vec<String>>().join(" ");
-    let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    say(message.as_bytes(), 32, &mut writer).unwrap();
 }
